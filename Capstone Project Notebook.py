278fb36aa8711137f8af67cf559b243ab3f18685
patra0403 <41643942+patra0403@users.noreply.github.com>
2019-07-30 14:51:31 -0500

    Capstone Project Notebook

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b> Capstone Project </b>
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course! ")
+
+
+# In[ ]:
+
+
+
+
